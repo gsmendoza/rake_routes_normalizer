a81a55016f07e1ee5ef52ba112413c2d98eb6969
George Mendoza <gsmendoza@gmail.com>
2011-12-07 15:04:11 +0800

    [#3] Rearrange printout so its easier to read.

--- lib/rake_routes_normalizer/app.rb
+++ lib/rake_routes_normalizer/app.rb
@@ -3,7 +3,7 @@ module RakeRoutesNormalizer
     desc "RAKE_PRINTOUT_FILE", "Print the normalized version of RAKE_PRINTOUT_FILE"
     def normalize(text)
       table = RouteSet.parse(text).normalize.routes.map do |route|
-        [route.name, route.http_verb, route.url_pattern, route.params.inspect]
+        [route.url_pattern, route.http_verb, route.name, route.params.inspect]
       end
       print_table table
     end
